9162ddd3b32fb204040a730a1c120cbca4ea52a8
zhangshiwei <zsw@qq.com>
2022-08-04 21:25:31 +0800

    GitTest2.java

---
+++ src/main/java/com/atguigu/GitTest2.java
@@ -0,0 +1,10 @@
+package com.atguigu;
+
+public class GitTest2 {
+    public static void main(String[] args) {
+        System.out.println("args = " + args);
+        System.out.println("args = " + args);
+        System.out.println("args = " + args);
+        System.out.println("args = " + args);
+    }
+}
